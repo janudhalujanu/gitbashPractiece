90c12a902f08cf36f8f38bff4f24aa8c32dc56fb
janudhalujanu <nagarajujyothi98@gmail.com>
2023-05-15 16:27:17 +0530

    2nd

--- src/test/java/Practice/Pro.java
+++ src/test/java/Practice/Pro.java
@@ -1,5 +1,12 @@
 package Practice;
 
+import org.testng.annotations.Test;
+
 public class Pro {
+	@Test
+	public void SetUp() {
+		System.out.print("gitbsh");
+		System.out.println("hello");
+	}
 
 }
